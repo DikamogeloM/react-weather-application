9aea7ab863e01c75ff509f3308eb7c356cb7f69c
DikamogeloM <dikamogelom@gmail.com>
2024-09-10 23:22:31 +0200

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>React Weather App </h1>
-      <footer>
-        This project was coded by Dikamogelo Mabusela and is open-sourced on{" "}
-        <a
-          href="https://github.com/DikamogeloM/react-weather-application"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>React Weather App </h1>
+
+        <footer>
+          This project was coded by Dikamogelo Mabusela and is open-sourced on{" "}
+          <a
+            href="https://github.com/DikamogeloM/react-weather-application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
