5d29c65bfefe5ab37daa0d5f4714252ded6b83d1
DikamogeloM <dikamogelom@gmail.com>
2024-09-10 23:08:43 +0200

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Weather App </h1>
+      <footer>
+        This project was coded by Dikamogelo Mabusela and is open-sourced on{" "}
+        <a
+          href="https://github.com/DikamogeloM/react-weather-application"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github{" "}
+        </a>
+      </footer>
     </div>
   );
 }
