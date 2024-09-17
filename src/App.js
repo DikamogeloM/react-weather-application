6e89ba546655553bf7838bce3456b9bcd7c05f2b
DikamogeloM <dikamogelom@gmail.com>
2024-09-17 23:50:43 +0200

    Refactored code and fixed warnings

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Pretoria" />
         <footer>
-          This project was coded by Dikamogelo Mabusela and is open-sourced on{" "}
+          This project was coded by{" "}
+          <a
+            href="https://github.com/DikamogeloM"
+            target="_blank"
+            rel="noopener noreferrer "
+          >
+            Dikamogelo Mabusela
+          </a>{" "}
+          and is open-sourced on{" "}
           <a
             href="https://github.com/DikamogeloM/react-weather-application"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
-            Github{" "}
+            Github,{" "}
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://app.netlify.com/sites/clinquant-bonbon-5d1d6d/overview"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify.
           </a>
         </footer>
       </div>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -7,13 +7,25 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "4fft9ab4oba0fa5c5b989ee6eb4b9143";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
-    console.log(forecast);
     return (
       <div className="WeatherForecast">
         <div className="row">
@@ -24,18 +36,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "4fft9ab4oba0fa5c5b989ee6eb4b9143";
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
